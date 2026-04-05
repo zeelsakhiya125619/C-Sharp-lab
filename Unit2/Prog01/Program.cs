fb90a937ede9e155416977289d0b9a365d7f7bf0
zeelsakhiya125619 <zeel.sakhiya125619@marwadiuniversity.ac.in>
2026-04-05 16:34:26 +0530

    Add files via upload

---
+++ Unit2/Prog01/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+    Definittion 1
+    Write a program to print "Hello World!" on the console.
+*/
+
+using System;
+
+namespace Prog01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Unit2/Prog02/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+    Definition 2
+    Write a program to input 2 number and an arithmetic  operator. Display the result accordingly. 
+*/
+
+using System;
+
+namespace Prog02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter number 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int addition = num1 + num2;
+            int subtraction = num1 - num2;
+            int multiplication = num2 * addition;
+            int division = num1 / num2;
+
+            Console.WriteLine("Addition: " + addition
+                              + "\nSubtraction: " + subtraction
+                              + "\nMultiplication: " + multiplication
+                              + "\nDivision: " + division);
+        }
+    }
+}
